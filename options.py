229138b6e63c71c288ebf11a3bdf9ec60eb4ad52
Alen_Jumaev <jumaevalen3354@gmail.com>
2023-06-03 20:27:15 +0500

    second win

--- options.py
+++ options.py
@@ -1,20 +1,42 @@
+from PyQt5.QtCore import QTime
+
 win_width = 1850
 win_height = 1300
 pulse_icon = 'pulse.png'
 win_title = 'Rufier-test - by Alen Jumayev and Olzhas Amankeldin'
 hello = 'Welcome to the health assessment program!'
-txt_instruction = 'This application will allow you to use the Rufier test to conduct a primary '\
-                  'diagnosis of your health.\n'\
-                  'The Rufier test is a load complex designed to assess performance '\
-                  'hearts during physical exertion.\n'\
-                  'The pulse rate is determined in the subject who is lying on his back for 5 '\
-                  'minutes in 15 seconds;\n'\
-                  'then, within 45 seconds, the subject performs 30 squats.\n'\
-                  'After the end of the load, the subject lies down, and the number of pulsations '\
-                  'for him is counted again.'\
-                  'the first 15 seconds,\n'\
-                  'and then — for the last 15 seconds of the first minute of the recovery period.\n'\
-                  'Important! If you feel unwell during the test\n'\
-                  '(dizziness will appear, tinnitus, severe shortness of breath, etc.)\n'\
+txt_instruction = 'This application will allow you to use the Rufier test to conduct a primary ' \
+                  'diagnosis of your health.\n' \
+                  'The Rufier test is a load complex designed to assess performance ' \
+                  'hearts during physical exertion.\n' \
+                  'The pulse rate is determined in the subject who is lying on his back for 5 ' \
+                  'minutes in 15 seconds;\n' \
+                  'then, within 45 seconds, the subject performs 30 squats.\n' \
+                  'After the end of the load, the subject lies down, and the number of pulsations ' \
+                  'for him is counted again.' \
+                  'the first 15 seconds,\n' \
+                  'and then — for the last 15 seconds of the first minute of the recovery period.\n' \
+                  'Important! If you feel unwell during the test\n' \
+                  '(dizziness will appear, tinnitus, severe shortness of breath, etc.)\n' \
                   'then the test should be interrupted and consult a doctor.'
 button_next = 'Next'
+name = 'Write your full name: '
+age = 'Age: '
+test1 = 'Lie on your back and measure your pulse in 15 seconds. ' \
+        'Click the "Start First Test" button to start the timer '\
+        'Write the result in the appropriate field.'
+test2 = 'Perform 30 squats in 45 seconds. ' \
+        'To do this, click the "Start doing squats" button to start the squat counter.'
+test3 = 'Lie on your back and measure your pulse first for the first 15 seconds of the minute, ' \
+        'then in the last 15 seconds. ' \
+        'Click the "Start Final test" button, to start the timer.\n' \
+        'Green indicates the seconds during which it is necessary\n' \
+        'take measurements, black - seconds without measuring pulsations. ' \
+        'Write down the results in the appropriate fields.'
+time = QTime(0, 0, 15)
+timer = time.toString("hh:mm:ss")
+hintname = "Full name"
+hintage = "0"
+txt_hinttest1 = '0'
+txt_hinttest2 = '0'
+txt_hinttest3 = '0'
--- second_win.py
+++ second_win.py
@@ -2,6 +2,17 @@ from PyQt5 import QtGui
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import *
 from options import *
+from third_win import *
+from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
+from PyQt5.QtGui import *
+
+
+class Experiment():
+   def __init__(self, age, test1, test2, test3):
+       self.age = age
+       self.t1 = test1
+       self.t2 = test2
+       self.t3 = test3
 
 
 class SecondWin(QWidget):
@@ -18,14 +29,108 @@ class SecondWin(QWidget):
         self.setWindowIcon(QtGui.QIcon(pulse_icon))
 
     def initUI(self):
-        self.hello = QLabel(hello)
+        self.text_name = QLabel(name)
+        self.text_age = QLabel(age)
+        self.text_test1 = QLabel(test1)
+        self.text_test2 = QLabel(test2)
+        self.text_test3 = QLabel(test3)
+        self.text_timer = QLabel(timer)
+        self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
+        self.line_name = QLineEdit(hintname)
+        self.line_age = QLineEdit(hintage)
+        self.line_test1 = QLineEdit(txt_hinttest1)
+        self.line_test2 = QLineEdit(txt_hinttest2)
+        self.line_test3 = QLineEdit(txt_hinttest3)
 
-        self.vline = QVBoxLayout()
-        self.vline.addWidget(self.hello, alignment=Qt.AlignCenter | Qt.AlignTop)
-        self.setLayout(self.vline)
+        self.l_line = QVBoxLayout()
+        self.r_line = QVBoxLayout()
+        self.h_line = QHBoxLayout()
+        self.r_line.addWidget(self.text_timer, alignment=Qt.AlignCenter)
+        self.l_line.addWidget(self.text_name, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.line_name, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.text_age, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.line_age, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.text_test1, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.btn_test1, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.line_test1, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.text_test2, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.btn_test2, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.text_test3, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.btn_test3, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.line_test2, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.line_test3, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.btn_next, alignment=Qt.AlignCenter)
+        self.h_line.addLayout(self.l_line)
+        self.h_line.addLayout(self.r_line)
+        self.setLayout(self.h_line)
+
+    def next_click(self):
+        self.hide()
+        self.exp = Experiment(int(self.line_age.text()), self.line_test1.text(), self.line_test2.text(),
+                              self.line_test2.text())
+        self.fw = FinalWin(self.exp)
+
+    def timer_test(self):
+        global time
+        time = QTime(0, 0, 15)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer1Event)
+        self.timer.start(1000)
+
+    def timer_sits(self):
+        global time
+        time = QTime(0, 0, 30)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer2Event)
+        # одно приседание в 1.5 секунды
+        self.timer.start(1500)
+
+    def timer_final(self):
+        global time
+        time = QTime(0, 1, 0)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer3Event)
+        self.timer.start(1000)
+
+    def timer1Event(self):
+        global time
+        time = time.addSecs(-1)
+        self.text_timer.setText(time.toString("hh:mm:ss"))
+        self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
+        self.text_timer.setStyleSheet("color: rgb(0,0,0)")
+        if time.toString("hh:mm:ss") == "00:00:00":
+            self.timer.stop()
+
+    def timer2Event(self):
+        global time
+        time = time.addSecs(-1)
+        self.text_timer.setText(time.toString("hh:mm:ss")[6:8])
+        self.text_timer.setStyleSheet("color: rgb(0,0,0)")
+        self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
+        if time.toString("hh:mm:ss") == "00:00:00":
+            self.timer.stop()
+
+    def timer3Event(self):
+        global time
+        time = time.addSecs(-1)
+        self.text_timer.setText(time.toString("hh:mm:ss"))
+        if int(time.toString("hh:mm:ss")[6:8]) >= 45:
+            self.text_timer.setStyleSheet("color: rgb(0,255,0)")
+        elif int(time.toString("hh:mm:ss")[6:8]) <= 15:
+            self.text_timer.setStyleSheet("color: rgb(0,255,0)")
+        else:
+            self.text_timer.setStyleSheet("color: rgb(0,0,0)")
+        self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
+        if time.toString("hh:mm:ss") == "00:00:00":
+            self.timer.stop()
 
     def connects(self):
-        pass
+        self.btn_next.clicked.connect(self.next_click)
+        self.btn_test1.clicked.connect(self.timer_test)
+        self.btn_test2.clicked.connect(self.timer_sits)
+        self.btn_test3.clicked.connect(self.timer_final)
 
     def next_win(self):
         pass
+        # self.hide()
+        # self.mv = FinalWin()
